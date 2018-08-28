9ca4d3eb4eaeb3aa959c5f3cbbb846a1bd968c62
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-08-28 14:01:14 +0300

    test

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Paper from '@material-ui/core/Paper'
 import { withStyles } from '@material-ui/core/styles'
 import formStyles  from './formStyles'
+
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
